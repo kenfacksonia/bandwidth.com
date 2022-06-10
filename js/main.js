f8aafbf578f6d7273c6f58cca250e8f0aa863d13
Your Name <you@example.com>
2022-06-10 19:14:37 +0200

    site de bandwidth.com

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+//var myCarousel = document.querySelector('#myCarousel')
+//var carousel = new bootstrap.Carousel(myCarousel ,{
+    //interval: 4000,
+    //});
+//var myCarousel = document.getElementById('myCarousel')
+
+//myCarousel.addEventListener('slide.bs.carousel', function () {
+  // do something... 
+//});
+//setInterval("ChangeSlide(1)", 4000);
+//let slideIndex = [1,1];
+/* Class the members of each slideshow group with different CSS classes */
+//let slideId = ["mySlides1", "mySlides2"]
+//showSlides(1, 0);
+//showSlides(1, 1);
+
+//function plusSlides(n, no) {
+  //showSlides(slideIndex[no] += n, no);
+//}
+
+//function showSlides(n, no) {
+  //let i;
+  //let x = document.getElementsByClassName(slideId[no]);
+  //if (n > x.length) {slideIndex[no] = 1}
+  //if (n < 1) {slideIndex[no] = x.length}
+  //for (i = 0; i < x.length; i++) {
+   // x[i].style.display = "none";
+//  }
+ // x[slideIndex[no]-1].style.display = "block";
+//}
+
+  
